dec9ff37b11a3cde29390b0572ead51f4e376960
cristiano250 <50516718+cristiano250@users.noreply.github.com>
2022-04-29 14:42:46 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,5 @@ module.exports = {
   transpileDependencies: true,
   lintOnSave: false,
   runtimeCompiler: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/BookstoreAppFinal/" : "/",
 }
